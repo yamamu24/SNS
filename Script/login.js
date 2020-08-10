8c32df091b80d823bab0faedd5d31ebf8b9f836f
yamamu24 <yamamu24@gmail.com>
2020-08-10 19:53:07 +0900

    実装修正

---
+++ Script/login.js
@@ -0,0 +1,3 @@
+function registUser() {
+    $('#registForm').submit();
+}
--- Views/login.php
+++ Views/login.php
@@ -1,9 +1,23 @@
+<?php
+	require("../Model/dbconnect.php");
+
+	if (!empty($_POST)) {
+		$email = $_POST["email"];
+		$password = $_POST["password"];
+
+		if ($email !== '' && $password !== '') {
+			
+		}
+	}
+?>
+
 <!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <script type="text/javascript" sorce="../Script/login.js"></script>
+    <script type="text/javascript" src="../Script/login.js"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
     <link rel="stylesheet" href="../CSS/login.css">
 </head>
 <body>
@@ -12,16 +26,24 @@
     </div>
 
     <div class="content">
-        <form action="" method="post">
-        <div>
-            <span>メールアドレス</span>
-            <input type="text">
-        </div>
+		<?php if (!empty($error)): ?>
+			<div>
+				test
+			</div>
+		<?php endif; ?>
+
+        <form id="registForm" action="" method="post">
+			<div>
+				<span>メールアドレス</span>
+				<input type="text" name="email">
+			</div>
             
-        <div>
-            <span>パスワード</span>
-            <input type="password">
-        </div>
+			<div>
+				<span>パスワード</span>
+				<input type="password" name="password">
+			</div>
+
+			<input type="button" value="登録" onclick="registUser()">
         </form>
     <div>
 </body>
