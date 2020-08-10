6b1a9f7e22e144664a21fb8bdb5c9557a34fef3b
yuto.yamamura <yamamu24@gmail.com>
2020-08-10 17:15:35 +0900

    ログイン画面用ファイル追加

---
+++ Views/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../CSS/index.css">
+</head>
+<body>
+    <div id="header">
+        <div>やまったー</div>
+    </div>
+</body>
---
+++ Views/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script type="text/javascript" sorce="../Script/login.js"></script>
+    <link rel="stylesheet" href="../CSS/login.css">
+</head>
+<body>
+    <div id="header">
+        <div>やまったー</div>
+    </div>
+
+    <div class="content">
+        <form action="" method="post">
+        <div>
+            <span>メールアドレス</span>
+            <input type="text">
+        </div>
+            
+        <div>
+            <span>パスワード</span>
+            <input type="password">
+        </div>
+        </form>
+    <div>
+</body>
